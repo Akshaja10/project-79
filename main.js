83fe8113fc725a7a0146a25a2ea57144da727d15
Akshaja10 <76399647+Akshaja10@users.noreply.github.com>
2021-02-15 12:04:21 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+menu_list_array=["Chicken Tandoori Pizza",
+                 "Veg Supreme Pizza",
+                  "Paneer Tikka Pizza",
+                  "Deluxe Veggie Pizza",
+                  "Veg Extavaganza Pizza"];
+
+function getmenu() {
+var htmldata;
+htmldata="<ol class='menulist'>"
+menu_list_array.sort();
+for (var i=0;i <menu_list_array.length;i++) {
+htmldata-htmldata+ '<li>' +menu_list_array [i] + '</li>'
+
+}
+
+htmldata=htmldata+ "</ol>"
+document.getElementById ("display_menu").innerHTML=htmldata;
+
+}
+
+function add_item() {
+var htmldata;
+var item=document.getElementsByClassName("add_item").value;
+menu_list_array.push (item);
+menu_list_array.sort();
+htmldata="<section class='cards'>"
+for (var i=0;i <menu_list_array.length;i++) {
+
+    htmldata=htmldata+ "<div class='cards'>" + '<img src="images/pizzalmg.png"/>' + menu_list_array[1] + '</div>'
+}
+htmldata=htmldata+"</section>"
+document.getElementById ("display_addedmenu").innerHTML=htmldata;
+}
